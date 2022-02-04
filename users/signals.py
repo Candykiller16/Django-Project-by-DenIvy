1afaa0ebbc72bf57c96bf32d012e7ef2517cda93
Candykiller16 <Candykiller16@mail.ru>
2022-02-04 16:01:51 +0300

    third commit

---
+++ users/signals.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from .models import Profile
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
+#@receiver(post_save, sender=Profile)
+def createProfile(sender, instance, created, **kwargs):
+    print('Profile signal triggered')
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name,
+        ) # if we create user we automatically create profile for user and fill some fields of profile
+
+
+
+def delete_user(sender, instance, **kwargs):
+    user = instance.user
+    user.delete()
+    print('Deleting user...') # if we delete user's profile, we delete user too
+
+post_save.connect(createProfile, sender=User)
+post_delete.connect(delete_user, sender=Profile)
