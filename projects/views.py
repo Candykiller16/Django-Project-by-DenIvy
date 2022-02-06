558071e712a61eda8926efe7138bda6c3867e693
Candykiller16 <Candykiller16@mail.ru>
2022-02-06 14:46:13 +0300

    fourth commit

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
-
+from django.contrib.auth.decorators import login_required
 
 def projects(request):
     projects = Project.objects.all()
@@ -17,7 +17,7 @@ def project(request, pk):
                }
     return render(request, 'projects/single-project.html', context)
 
-
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     if request.method == 'POST':
@@ -29,7 +29,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
-
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)  # parameter to show which project we want to update
@@ -43,7 +43,7 @@ def updateProject(request, pk):
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
-
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
 
--- users/views.py
+++ users/views.py
@@ -1,8 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+def loginUser(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username doesn't exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username OR Password is incorrect')
+    return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, "Username was successfully logged out")
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+    context = {'page': page, 'form': form}
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created')
+
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.success(request, 'An error occurred during registration')
+
+    return render(request, 'users/login_register.html', context)
+
 
 def profiles(request):
     profiles = Profile.objects.all()
@@ -10,7 +66,7 @@ def profiles(request):
     return render(request, 'users/profiles.html', context)
 
 
-def uerProfile(request, pk):
+def userProfile(request, pk):
     profile = Profile.objects.get(id=pk)
 
     topSkills = profile.skill_set.exclude(description="")  # skills that have discription
@@ -18,3 +74,14 @@ def uerProfile(request, pk):
 
     context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
+
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+
+    skills = profile.skill_set.all() # all skills
+    projects = profile.project_set.all()
+
+    context = {'profile': profile, 'skills': skills, 'projects': projects}
+    return render(request, 'users/account.html', context)
